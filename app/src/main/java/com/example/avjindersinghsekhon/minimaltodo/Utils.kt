99da0f68268e6be49ce3ece5ad59aaa1218a18f1
yongju <yong8521@gmail.com>
2017-07-23 13:22:06 +0900

    #11 Utils 클래스 코틀린으로 변환  - run 적용.

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Utils.kt
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Utils.kt
@@ -6,29 +6,14 @@ import android.content.Context
  * Created by yongju on 2017. 7. 19..
  */
 
-
-/*
-//original source
-public class Utils {
-        public static int getToolbarHeight(Context context) {
-            final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
-                    new int[]{R.attr.actionBarSize});
-            int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
-            styledAttributes.recycle();
-
-            return toolbarHeight;
-        }
-}
- */
-
 class Utils {
     companion object {
         @JvmStatic
-        fun getToolbarHeight(context: Context): Int {
-            val obtainStyledAttributes = context.theme.obtainStyledAttributes(kotlin.IntArray(1){R.attr.actionBarSize})
-            val dimension = obtainStyledAttributes.getDimension(0, 0.0f)
-            obtainStyledAttributes.recycle()
-            return dimension.toInt()
+        fun getToolbarHeight(context: Context)
+                = context.theme.obtainStyledAttributes(kotlin.IntArray(1) { R.attr.actionBarSize }).run {
+            val dim = getDimension(0, 0f)
+            recycle()
+            dim.toInt()
         }
     }
 }
