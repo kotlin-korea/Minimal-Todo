1694bf93b5e29844cb7ff1c5239d8d9c78df3af2
Patternoid <patternoid@Patternoidui-MacBook-Air.local>
2017-07-26 06:46:39 +0900

    주석 삭제

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/DeleteNotificationService.kt
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/DeleteNotificationService.kt
@@ -39,8 +39,8 @@ class DeleteNotificationService : IntentService {
                 saveData()
             }
         }
-
     }
+
 
     private fun dataChanged(){
         val sharedPreferences : SharedPreferences = getSharedPreferences(MainActivity.SHARED_PREF_DATA_SET_CHANGED, Context.MODE_PRIVATE)
@@ -82,77 +82,3 @@ class DeleteNotificationService : IntentService {
         return null
     }
 }
-
-
-
-
-/*
-public class DeleteNotificationService extends IntentService {
-
-    private StoreRetrieveData storeRetrieveData;
-    private ArrayList<ToDoItem> mToDoItems;
-    private ToDoItem mItem;
-
-    public DeleteNotificationService(){
-        super("DeleteNotificationService");
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        storeRetrieveData = new StoreRetrieveData(this, MainActivity.FILENAME);
-        UUID todoID = (UUID)intent.getSerializableExtra(TodoNotificationService.TODOUUID);
-
-        mToDoItems = loadData();
-        if(mToDoItems!=null){
-            for(ToDoItem item : mToDoItems){
-                if(item.getIdentifier().equals(todoID)){
-                    mItem = item;
-                    break;
-                }
-            }
-
-            if(mItem!=null){
-                mToDoItems.remove(mItem);
-                dataChanged();
-                saveData();
-            }
-
-        }
-
-    }
-
-    private void dataChanged(){
-        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.SHARED_PREF_DATA_SET_CHANGED, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(MainActivity.CHANGE_OCCURED, true);
-        editor.apply();
-    }
-
-    private void saveData(){
-        try{
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        saveData();
-    }
-
-    private ArrayList<ToDoItem> loadData(){
-        try{
-            return storeRetrieveData.loadFromFile();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return null;
-
-    }
-}
-
- */
